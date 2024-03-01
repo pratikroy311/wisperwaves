f64004781a2efa34c621d3ef301c55a6b84169e0
pratik <pratikroy311@gmail.com>
2024-03-01 18:41:27 +0530

    creating api endpoint for filtering servers by server id

--- wisperwaves/server/views.py
+++ wisperwaves/server/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from server.models import Server
 from server.serializer import ServerSerializer
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import AuthenticationFailed
 
 
 class ServerListViewSet(viewsets.ViewSet):
@@ -11,6 +13,10 @@ class ServerListViewSet(viewsets.ViewSet):
         category = request.query_params.get('category')
         qty = request.query_params.get('qty')
         by_user = request.query_params.get("by_user")=="true"
+        by_server_id= request.query_params.get("by_server_id")
+        
+        if by_user or by_server_id and not request.user.is_authenticated:
+            raise AuthenticationFailed()
         
         if category:
             self.queryset= self.queryset.filter(category__name=category)
@@ -21,7 +27,14 @@ class ServerListViewSet(viewsets.ViewSet):
         
         if qty:
             self.queryset= self.queryset[: int(qty)]
-            
+        if by_server_id:
+            try:
+                self.queryset = self.queryset.filter(id=by_server_id)
+                if not self.queryset.exists():
+                    raise ValidationError(detail=f"server id: {by_server_id} not found")
+            except ValueError:
+                raise ValidationError(detail=f"server id is not valid")
+                          
         serializer = ServerSerializer(self.queryset,many=True)
         return Response(serializer.data)
             
