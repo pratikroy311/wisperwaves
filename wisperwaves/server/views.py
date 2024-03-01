67d3b89d7f224c035fc3343fa3025d343412e0d3
pratik <pratikroy311@gmail.com>
2024-03-01 17:59:48 +0530

    creating api endpoint for filtering servers by qty

--- wisperwaves/server/views.py
+++ wisperwaves/server/views.py
@@ -9,9 +9,12 @@ class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
     def list(self,request):
         category = request.query_params.get('category')
-        
+        qty = request.query_params.get('qty')
         if category:
             self.queryset= self.queryset.filter(category__name=category)
+        
+        if qty:
+            self.queryset= self.queryset[: int(qty)]
     
         serializer = ServerSerializer(self.queryset,many=True)
         return Response(serializer.data)
