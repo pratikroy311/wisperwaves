1d0474e9032d448f2ad8b9fe6f43a221d787eb5a
pratik <pratikroy311@gmail.com>
2024-03-03 16:56:59 +0530

    modifying serializer to return channel related data

--- wisperwaves/server/models.py
+++ wisperwaves/server/models.py
@@ -32,7 +32,7 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL,
                               on_delete=models.CASCADE,related_name='channel_owner')
     topic = models.CharField(max_length=100)
-    server = models.ForeignKey(Server,on_delete=models.CASCADE)
+    server = models.ForeignKey(Server,on_delete=models.CASCADE,related_name="channel_server")
     
     def save(self, *args, **kwargs):
         self.name = self.name.lower()
--- wisperwaves/server/serializer.py
+++ wisperwaves/server/serializer.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from server.models import Category, Server
+from server.models import Category, Server, Channel
 
+
+class  ChannelSerializer(serializers.Serializer):
+    class Meta:
+        model = Channel
+        firlds = "__all__"
+        
 class ServerSerializer(serializers.ModelSerializer):
+    channel_server = ChannelSerializer(many=True)
     
     class Meta:
         model = Server
